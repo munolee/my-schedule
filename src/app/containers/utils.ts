c3a65d8aa2e10f4c863b4c1a052f25e69bf52850
munouse <munouse0402@gmail.com>
2021-03-07 20:52:27 +0900

    code refactor

--- src/app/containers/utils.ts
+++ src/app/containers/utils.ts
@@ -271,54 +271,6 @@ export function handlePopup(i: any, data: any) {
         }
     });
     if (data.classActive !== 'active') {
-        if (data.type.includes('hospitalDelete')) { //# 병원 정보 삭제
-            i.handleHospitalDelete(data.type.replace('hospitalDelete', ''));
-        }
-        if (data.type.includes('addressDelete')) { //# 지역 정보 삭제
-            i.handleAddressDelete(data.type.replace('addressDelete', ''));
-        }
-        if (data.type === 'editPassword') { //# 비밀번호 변경
-            i.handleEditPasswordData();
-        }
-        if (data.type === 'complete') { //# Result Success
-            window.history.back();
-        }
-        if (data.type === 'registerHospital') {
-            history.push('/hospital/register');
-        }
-        if (data.type === 'bid') { //## 입찰 전 유의사항 팝업 결과 체크
-            i.handleBidPopupShow();
-        }
-        if (data.type.includes('bidCancel')) { //# 환자 입찰 취소하기
-            i.applicantCancelApi(data.type.replace('bidCancel', ''));
-        }
-        if (data.type === 'applicantComplete') { //# 환자 지원하기
-            history.push('/patient/list');
-        }
-        if (data.type.includes('careStart')) { //# 간병 시작
-            i.handleCareStart(data.type.replace('careStart', ''));
-        }
-        if (data.type === 'accountCheck') { //# 계좌 등록
-            i.handleAccountRegister();
-        }
-        if (data.type.includes('accountDelete')) { //# 계좌 삭제
-            i.accountDeleteApi(data.type.replace('accountDelete', ''));
-        }
-        if (data.type === 'transferCheck') { //# 계좌 확인
-            history.push('/bankbook/transfer/info', {
-                bank_code: i.state.bankList.code,
-                bank_name: i.state.bankList.name,
-                bank_number: i.state.accountNumber,
-                amount: i.state.transferAmount.replace(/,/g, ""),
-                receiver: i.state.transferName
-            });
-        }
-        if (data.type === 'initPin') { //# 이체비밀번호 등록
-            history.push('/user/pin');
-        }
-        if (data.type === 'pinInit') { //# 이체비밀번호 초기화
-            i.pinRef.current?.clear();
-        }
         if (data.type === 'login') { //# 로그인 페이지 이동
             history.push('/login');
         }
@@ -352,37 +304,6 @@ export function handleData(i: any, e: any) {
     });
 }
 
-/**
- * Data Set State
- * ---------------------------------------------------------------------------------------------------------------------
- *
- * @param i : this
- * @param e : Object Data
- */
-export function handleAmount(i: any, e?: any) {
-    if (isEmpty(e.target)) {
-        return
-    }
-    if (e.target.name == null) {
-        return;
-    }
-    let value = e.target.value;
-    let subStr = value.substr(value.length - 1);
-
-    if (!isEmpty(subStr) && !isAmount(subStr)) {
-        value = value.substr(0, value.length - 1); //## 마지막 문자 제거
-        i.setState({
-            [e.target.name]: value,
-            ...e
-        });
-    } else {
-        i.setState({
-            [e.target.name]: value,
-            ...e
-        });
-    }
-}
-
 
 /**
  * Data Set State
@@ -396,35 +317,6 @@ export function handleDeleteData(i: any, data: any) {
         ...i.state,
         ...data
     });
-}
-
-/**
- * 공고 상태 색상
- * ---------------------------------------------------------------------------------------------------------------------
- *
- * @param history : job Detail
- */
-export function jobStatusColor(history: any) {
-    /*
-        1. 신청대기 / 간병 대기 : 노랑색
-        2. 간병 중 : 파랑색
-        3. 간병 완료 : 회색
-        4. 취소요청 / 취소완료 : 빨간색
-     */
-    let colorName;
-    if (history.status === 3 || history.status === 1 || history.status === 2) {
-        colorName = 'yw_ver';
-    } else if (history.status === 4) {
-        colorName = 'blue_ver';
-    } else if (history.status === 5) {
-        colorName = 'gray_ver';
-    } else if (history.status === 9) {
-        colorName = 'red_ver';
-    }
-    if (history.cancel_status === 'R' || history.cancel_status === 'Y') {
-        colorName = 'red_ver';
-    }
-    return colorName;
 }
 
 /**
@@ -475,52 +367,5 @@ export function adjustType(value: string) {
         };
         const iosHandler = iosDevice('adjustType');
         iosHandler.postMessage(iosData);
-    }
-}
-
-/**
- * 주민등록번호, 외국인등록번호 유효성 검사
- * ---------------------------------------------------------------------------------------------------------------------
- */
-export function rrnValidationCheck(rrn: string, foreigner: number) {
-    //### 주민등록증 검증 조건
-    //# 주민/외국인 등록번호는 13자리의 숫자로 이루어져 있음
-    //# 앞의 6자리는 생년월일을 의미하고 뒤의 7자리는 구글에 검색
-    //# 주민/외국인 등록번호의 앞 12자리 숫자를 앞에서부터 2,3,4,5,6,7,8,9,2,3,4,5를 곱하고
-    //# 주민등록번호는 11에서 검증값합계의 11의 나머지수를 뺀 값의 1의 자리 수가 12번째 숫자랑 일치해야함
-    //# 외국인등록번호는 13에서 검증값합계의 11의 나머지수를 뺀 값의 1의 자리수가 12번째 숫자랑 일치해야함
-
-    if (rrn === '000000-0000000') {
-        return true;
-    }
-
-    let rrnRegex = /^(?:[0-9]{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[1,2][0-9]|3[0,1]))-[1-4][0-9]{6}$/;
-    let frnRegex = /^(?:[0-9]{2}(?:0[1-9]|1[0-2])(?:0[1-9]|[1,2][0-9]|3[0,1]))-[5-6][0-9]{6}$/;
-    if (rrnRegex.test(rrn) || frnRegex.test(rrn)) {
-        let rrnStr: string = rrn.split("-").join('');
-        if (rrnStr.length !== 13) return false;
-
-        let checkSum = 0;
-        for (let i = 0; i < 12; i++) {
-            checkSum += ((Number(rrnStr.substr(i, 1)) >> 0) * ((i % 8) + 2));
-        }
-
-        if (foreigner === 0 && rrnRegex.test(rrn)) {//# 내국인
-            if ((11 - (checkSum % 11)) % 10 === Number(rrnStr.substr(12, 1))) {
-                return true;
-            } else {
-                return false;
-            }
-        } else if (foreigner === 1 && frnRegex.test(rrn)) {//# 외국인
-            if ((13 - (checkSum % 11)) % 10 === Number(rrnStr.substr(12, 1))) {
-                return true;
-            } else {
-                return false;
-            }
-        } else {
-            return false;
-        }
-    } else {
-        return false;
     }
 }
--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,21 +1,14 @@
 import React from "react";
 import {Router, Route, Switch, Redirect} from "react-router-dom";
-// import { Provider } from "mobx-react";
 
 import {
     Calendar,
     TimeTable
 } from "./containers";
 import history from "../app/containers/history";
-// import { Calendar } from "react-big-calendar";
-
-// import "../assets/css/main.css";
 // import RootStore from "./stores/rootStore";
 // import PrivateRoute from "./privateRoute";
-// import * as Utils from "./constants/utils";
 
-// const APP_SERVER_TYPE = ['REAL', 'QA'];
-// const SERVER_TYPE = process.env.REACT_APP_SERVER_TYPE;
 // const rootStore = new RootStore();
 const Root: React.FC = () => (
     <Router history={history}>
