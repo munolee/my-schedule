58a3b8a94339ff61c1c9bb2ef711bef5dacb5e76
munouse <munouse0402@gmail.com>
2021-03-08 21:08:16 +0900

    temp build

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,25 +1,28 @@
 import React from "react";
-import {Router, Route, Switch, Redirect} from "react-router-dom";
-
 import {
-    Calendar,
-    TimeTable
-} from "./containers";
+  HashRouter,
+  BrowserRouter,
+  Route,
+  Switch,
+  Redirect,
+} from "react-router-dom";
+
+import { Calendar, TimeTable } from "./containers";
 import history from "../app/containers/history";
 // import RootStore from "./stores/rootStore";
 // import PrivateRoute from "./privateRoute";
 
 // const rootStore = new RootStore();
 const Root: React.FC = () => (
-    <Router history={history}>
-        <Switch>
-            <Route path="/" exact component={Calendar}/>
-            <Route exact={true} path="/calendar" component={Calendar}/>
-            <Route path="/table" component={TimeTable}/>
-            {/* <Route path="/error" component={Error} />
+  <HashRouter basename={process.env.PUBLIC_URL}>
+    {/* <Switch> */}
+    <Route path="/" component={Calendar} />
+    <Route exact={true} path="/calendar" component={Calendar} />
+    <Route path="/table" component={TimeTable} />
+    {/* <Route path="/error" component={Error} />
       <Redirect path="*" to="/error" /> */}
-        </Switch>
-    </Router>
+    {/* </Switch> */}
+  </HashRouter>
 );
 
 export default Root;
