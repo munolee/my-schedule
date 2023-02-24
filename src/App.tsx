d0de967596f1055edcddd951943d0a03463ba4c5
Ian <yyh_lee@fitpet.co.kr>
2023-02-24 16:38:07 +0900

    📦️ craco setting, path alias
    - craco 설치 및 path alias 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 // import Home from '@pages/Home';
 import Home from './pages/Home';
+import Header from '@components/common/header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header />
       <div className="App">
         <Routes>
           <Route path="/" element={<Home />} />
