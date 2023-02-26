903ed982f0fddd805d839e1deb297ac55b136e85
Ian <yyh_lee@fitpet.co.kr>
2023-02-26 22:19:55 +0900

    🩹 Event Paint 상태에 따라 마크업 요소 수정
    - ClassName으로 분기하던 style을 Props 값으로 분기하도록 수정

--- src/components/CalendarEventDate.tsx
+++ src/components/CalendarEventDate.tsx
@@ -1,6 +1,6 @@
 import { FC } from 'react';
 import styled from '@emotion/styled';
-import useEventSchedule from '@hooks/useEventSchedule';
+import useEventSchedule, { EventPaintEnum } from '@hooks/useEventSchedule';
 
 type CalendarEventDateProps = {
   calendarDate: string;
@@ -11,15 +11,22 @@ const CalendarEventDate: FC<CalendarEventDateProps> = ({ calendarDate }) => {
 
   return (
     <StyledEventList>
-      {currentMonthEvent.map((event, index) => (
-        <EventDateBar
-          key={index}
-          className={`${getEventPaintType(event, calendarDate)} event${index % 5}`}
-          // className={`${className} event${index % 5} ${index >= 4 ? 'hide' : ''}`}
-        >
-          {calendarDate === event.startDate ? event.eventTitle : ''}
-        </EventDateBar>
-      ))}
+      {currentMonthEvent.map((event, index) => {
+        const paintType = getEventPaintType(event, calendarDate);
+        if (paintType === EventPaintEnum.Empty) {
+          return;
+        }
+        return (
+          <EventDateBar
+            key={index}
+            paintType={paintType}
+            className={`${getEventPaintType(event, calendarDate)} event${index % 5}`}
+            // className={`${className} event${index % 5} ${index >= 4 ? 'hide' : ''}`}
+          >
+            {calendarDate === event.startDate ? event.eventTitle : ''}
+          </EventDateBar>
+        );
+      })}
     </StyledEventList>
   );
 };
@@ -33,7 +40,7 @@ const StyledEventList = styled.div`
   min-height: 80px;
 `;
 
-const EventDateBar = styled.span`
+const EventDateBar = styled.span<{ paintType: EventPaintEnum }>`
   position: relative;
   width: 100%;
   height: 16px;
@@ -43,34 +50,24 @@ const EventDateBar = styled.span`
   font-size: 12px;
   font-weight: 700;
   color: #000000;
-  border-radius: 100%;
+  border-radius: ${({ paintType }) => {
+    if (paintType === EventPaintEnum.StartDate) {
+      return '100px 0 0 100px';
+    }
+    if (paintType === EventPaintEnum.OneDay) {
+      return '100px';
+    }
+    if (paintType === EventPaintEnum.EndDate) {
+      return '0 100px 100px 0';
+    }
+    if (paintType === EventPaintEnum.Ing) {
+      return '0';
+    }
+    return '0';
+  }};
 
   &:hover {
     opacity: 0.8;
-  }
-  &.start {
-    border-radius: 100px 0 0 100px;
-  }
-  &.one-day {
-    border-radius: 100px;
-  }
-  &.end {
-    border-radius: 0 100px 100px 0;
-  }
-  &.ing {
-    border-radius: 0;
-  }
-  //&.empty {
-  //  background-color: rgba(0, 0, 0, 0);
-  //  border-radius: 0;
-  //}
-  //&.more {
-  //  padding: 0;
-  //  margin: 0;
-  //  height: 5px;
-  //}
-  &.empty {
-    display: none;
   }
   &.event0 {
     background: #cfdd8e;
