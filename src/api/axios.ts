2884e267e3eca2f2c8044e3d8792f8ba117833a2
Ian <yyh_lee@fitpet.co.kr>
2023-03-08 17:20:40 +0900

    ✏️ axios base Url 수정

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,8 +1,7 @@
 import Axios, { AxiosError, AxiosResponse } from 'axios';
 
 export const axios = Axios.create({
-  // baseURL: process.env.API_DOMAIN || 'https://schdule-express.vercel.app',
-  baseURL: process.env.API_DOMAIN || 'http://localhost:8080',
+  baseURL: process.env.API_DOMAIN || 'https://schdule-express.vercel.app',
   headers: {
     'content-type': 'application/json',
   },
